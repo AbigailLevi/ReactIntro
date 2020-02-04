2c8dd7e53b8170e50c2ba7946df7fa6c7a2eed7d
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-04 12:07:01 -0500

    sumtin New

---
+++ src/components/Buttton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-secondary">
+        Check me out fam
+      </button>
+    </div>
+  );
+};
+export default Button;
